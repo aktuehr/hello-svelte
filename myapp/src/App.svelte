5f192e4bc4014edfeba1871fe7b436beadd411ce
Akito UEHARA <akt.uehr@gmail.com>
2022-11-07 08:22:14 +0900

    tutorial: Styling

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -5,3 +5,12 @@
 
 <h1>Hello {name.toUpperCase()}!</h1>
 <img {src} alt="{name} dances." />
+<p>This is a paragraph.</p>
+
+<style>
+  p {
+    color: purple;
+    font-family: "Comic Sans MS", cursive;
+    font-size: 2em;
+  }
+</style>
