ffad653647464f25bff53b63857d5ca771e0443f
Akito UEHARA <akt.uehr@gmail.com>
2022-11-15 06:52:57 +0900

    tutorial: Else-if blocks

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -37,6 +37,8 @@
   function toggle() {
     user.loggedIn = !user.loggedIn;
   }
+
+  let x = 7;
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
@@ -57,6 +59,14 @@
   <button on:click={toggle}>Log out</button>
 {:else}
   <button on:click={toggle}>Log in</button>
+{/if}
+
+{#if x > 10}
+  <p>{x} is grater than 10</p>
+{:else if 5 > x}
+  <p>{x} is less than 5</p>
+{:else}
+  <p>{x} is between 5 and 10</p>
 {/if}
 
 <style>
